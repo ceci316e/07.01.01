e371c64c76e54c1df4f80509e5f5a86705e4c2b8
“Cecilie <ceci316e@stud.kea.dk>
2023-02-05 11:52:56 +0100

    dynamisk på produkt

--- produkt.js
+++ produkt.js
@@ -6,13 +6,18 @@ const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
 /* res er forkortelse for response */
 function hentData() {
   fetch(url)
-    .then((res) => res.json)
+    .then((res) => res.json())
     .then(visProdukt);
 }
 
 function visProdukt(produkt) {
   console.log(produkt);
   document.querySelector("#model").textContent = produkt.productdisplayname;
+  document.querySelector("#model2").textContent = produkt.productdisplayname;
+  document.querySelector("#mærke").textContent = produkt.brandname;
+  document.querySelector("#farve").textContent = produkt.basecolour;
+  document.querySelector("#produktionsår").textContent = produkt.productionyear;
+
   document.querySelector("#produktBillede").src = imagePath;
 }
 
